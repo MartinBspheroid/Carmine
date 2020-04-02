e40bcf7a3dd5175e56859d5790c4b3130ff77d3b
Martin Blasko <mblasko42@gmail.com>
2020-04-03 00:35:13 +0200

    Checking for all triggered and playing states. Egh...

--- Carmine/Carmine.py
+++ Carmine/Carmine.py
@@ -111,9 +111,11 @@ class Carmine:
         if clip.is_triggered == 1:
             state = 3
         _(str(clip.name) + " > state:" + str(state))
-        if(clip.canonical_parent.canonical_parent.devices[0].name is clip.name):
-            return
-        if(state is 2 and clip.name != ""):
+        if(state == 2 or state == 3):
+            if(clip.canonical_parent.canonical_parent.devices[0].name is clip.name):
+                  # skip this if names of loaded device is already same as clip we are launching
+                return
+        if(state == 2 and clip.name != ""):
             load_success = self.loadDevice(clip.name)
             if(load_success != True):
                 self.actions.append(lambda: self.loading_failed(clip))
